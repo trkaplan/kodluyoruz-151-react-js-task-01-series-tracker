5697bd1af6890f7eec0cc4b779e3f40e32fa1b08
eylultuncel <eylultuncel1999@gmail.com>
2022-01-07 21:33:15 +0300

    seeries tracker functions completed

--- src/SeriesTracker.js
+++ src/SeriesTracker.js
@@ -10,65 +10,61 @@ export function SeriesTracker(series) {
 
   this.add = function (serie) {
     this.series.push(serie);
-    //(If a serie has been watched) {
-    // Update the count of watched series: "numberOfWatched"
-    // Check for "lastSerie" property, set if we don't.
-    // Check for "lastSerie"'s finishedDate, if the serie's "finishedDate" prop is greater,
-    // set "lastSerie" prop to "serie" object.
-    //}
-    if (serie.isWatched) {
-    } else {
-      // If a serie hasn't been watched:
-      // Check if serie has "isCurrent" prop
-      // Check if we have a "currentSerie" property. Set if we don't.
-      // Check if we have a "nextSerie" property as well - if we didn't
-      // set the .currentSerie property, set the .nextSerie property.
-    }
 
-    //it should also update the number of series marked as watched / unwatched:
-    //"numberOfWatched" and "numberOfUnWatched"
+    if (serie.isWatched) {
+      this.numberOfWatched += 1;
+
+      if (this.lastSerie === undefined) {
+        this.lastSerie = serie;
+      } else {
+        let lastSerieFinishedDate = this.lastSerie.finishedDate.split(".").map(Number); // tempd = [09, 09 , 2019] dd mm yyyy
+        const lastSerieDate = new Date(lastSerieFinishedDate[2], lastSerieFinishedDate[1], lastSerieFinishedDate[0]);
+
+        let serieFinishedDate = serie.finishedDate.split(".").map(Number);
+        const serieDate = new Date(serieFinishedDate[2], serieFinishedDate[1], serieFinishedDate[0]);
+
+        if (serieDate > lastSerieDate) {
+          this.lastSerie = serie;
+        }
+      }
+
+    } else {
+      this.numberOfUnWatched+=1
+
+      if (serie.isCurrent && this.currentSerie === undefined) {
+        this.currentSerie = serie;
+      }
+      else if (this.nextSerie === undefined) {
+        this.nextSerie = serie;
+      }
+    }
   };
 
   //check to see if we have series to process
   if (series.length > 0) {
-    //Loop through all of the series in the "series" argument
-    //Use the .add function to handle adding series, so we keep counts updated.
+    series.forEach(serie => { this.add(serie); })
   }
 
   this.finishSerie = function () {
-    // find and update currently watching serie in "this.series" array
-    // update "lastSerie" with the finished one
-    // set "currentSerie" with the next one
-    // set new nextSerie value with the next one which has not been watched.
-    // update "numberOfWatched" and "numberOfUnWatched" props
+
+    this.numberOfWatched += 1;
+    this.numberOfUnWatched -=1;
+
+    const currentSerie = this.series.find(serie =>serie.id === this.currentSerie.id);
+    delete currentSerie.isCurrent;
+    currentSerie.isWatched = true;
+    currentSerie.finishedDate = new Date().getDate()
+
+    this.lastSerie = this.currentSerie;
+    this.currentSerie = this.nextSerie;
+
+    const current = this.series.findIndex( (serie) => serie.id === this.currentSerie.id );
+    this.series[current].isCurrent = true;
+    const nextIndex = this.series.findIndex(serie => !serie.isWatched && !serie.isCurrent);
+    this.nextSerie = this.series[nextIndex];
   };
+
   this.printSeriesReport = function () {
     fancyLogSeriesReport(this);
   };
 }
-
-// Case 1
-// -------------------------------------------------
-
-/* const mySeriesTracker = new SeriesTracker(series);
-mySeriesTracker.printSeriesReport(); */
-
-// Case 2
-// -------------------------------------------------
-
-/* const mySeriesTracker = new SeriesTracker(series);
-mySeriesTracker.finishSerie();
-mySeriesTracker.printSeriesReport(); */
-
-// Case 3
-// -------------------------------------------------
-
-/* const mySeriesTracker = new SeriesTracker(series);
-const newSerie = {
-  id: "9",
-  name: "Lost",
-  genre: "Adventure",
-  directorId: "4"
-};
-mySeriesTracker.add(newSerie);
-mySeriesTracker.printSeriesReport(); */
